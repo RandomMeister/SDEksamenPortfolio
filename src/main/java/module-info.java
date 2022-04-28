ca46a10d9743dc622238cf30d21600107d2f200e
RandomMeister <93247484+RandomMeister@users.noreply.github.com>
2022-04-28 15:51:59 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.sdeksamenportfolio {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.sdeksamenportfolio to javafx.fxml;
+    exports com.example.sdeksamenportfolio;
+    exports com.company;
+    opens com.company to javafx.fxml;
+}
